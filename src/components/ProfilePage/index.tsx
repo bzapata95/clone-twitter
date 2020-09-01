1c3dc678bae1e4ecf2cbc74605e455c9c7078180
Bryan Zapata <bmzc95@gmail.com>
2020-08-31 23:33:07 -0500

    part one

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+
+        <h1>Bryan Zapata</h1>
+        <h2>@bryanczapata</h2>
+
+        <p>
+          Developer at <a href="https://idbi.pe">@idbi</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Lima, Perú
+          </li>
+          <li>
+            <CakeIcon />
+            Nacido(a) en 13 de abril de 1995
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            siguiendo <strong>94</strong>
+          </span>
+
+          <span>
+            <strong>672</strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+
+      {/* <Feed /> */}
+    </Container>
+  );
+};
+
+export default ProfilePage;
